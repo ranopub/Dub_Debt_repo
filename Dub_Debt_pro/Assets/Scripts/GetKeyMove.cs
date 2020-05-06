eb4fda6927e22c5e113c374764d38abe1bff1e95
ranopub <ranopub@hotmail.co.jp>
2020-05-06 23:14:28 +0900

    sphereMove

---
+++ Dub_Debt_pro/Assets/Scripts/GetKeyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKeyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform Transform = this.transform;
+        Vector3 pos = Transform.position;
+        
+        if (Input.GetKey(KeyCode.D))
+        {    
+            pos.x += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {    
+            pos.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {    
+            pos.y += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {    
+            pos.y -= 0.05f;
+        }
+        Transform.position = pos;
+    }
+}
---
+++ Dub_Debt_pro/Assets/Scripts/SphereMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform Transform = this.transform;
+        Vector3 pos = Transform.position;
+        
+        if (Input.GetKey(KeyCode.D))
+        {    
+            pos.x += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {    
+            pos.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {    
+            pos.y += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {    
+            pos.y -= 0.05f;
+        }
+
+        if(pos.x > 5 )
+        {
+            pos.x = 5;
+        }
+        if(pos.x < -5 )
+        {
+            pos.x = -5;
+        }
+        if(pos.y > 5 )
+        {
+            pos.y = 5;
+        }
+        if(pos.y < -5 )
+        {
+            pos.y = -5;
+        }
+
+        Transform.position = pos;
+    }
+}
