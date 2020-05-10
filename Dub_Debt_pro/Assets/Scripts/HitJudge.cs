8794146e544ba4eecf98020a3845b709d65cb6e2
ranopub <ranopub@hotmail.co.jp>
2020-05-10 21:52:28 +0900

    hit&innerSpin

---
+++ Dub_Debt_pro/Assets/Scripts/HitJudge.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HitJudge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 SpherePos = this.transform.position;
+        Vector3 CubePos = GameObject.Find("Cube").transform.position;
+        CriAtomSource audio = (CriAtomSource)GetComponent("CriAtomSource");
+        if(Vector3.Distance( SpherePos, CubePos) < 0.75f )
+        {
+            Debug.Log("!!");
+            
+            audio.Play();   
+
+            Destroy (this.gameObject, 1.0f);
+        }
+    }
+}
---
+++ Dub_Debt_pro/Assets/Scripts/InnerSpin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform Transform = this.transform;
+        
+        
+        //if (Input.GetKey(KeyCode.D))
+        //{    
+        //    pos.x += 0.05f;
+        //}
+        if (Input.GetKey(KeyCode.A))
+        {    
+            Transform.Rotate(0,0.05f,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {    
+            Transform.Rotate(0,-0.05f,0);
+        }
+        //if (Input.GetKey(KeyCode.S))
+        //{    
+        //    pos.y -= 0.05f;
+        //}
+        //Transform.position = pos;
+    }
+}
